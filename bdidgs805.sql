f4ea21f70d01228d048ed11839a4da796d2d9f40
Saul_Villeda_Lozada <s.villeda@sdib.com.mx>
2026-02-27 21:58:12 -0600

    Base de datos actualzada 27Feb26

--- bdidgs805.sql
+++ bdidgs805.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80042 (8.0.42)
  File Encoding         : 65001
 
- Date: 24/02/2026 20:54:48
+ Date: 27/02/2026 21:57:10
 */
 
 SET NAMES utf8mb4;
@@ -29,7 +29,7 @@ CREATE TABLE `alembic_version`  (
 -- ----------------------------
 -- Records of alembic_version
 -- ----------------------------
-INSERT INTO `alembic_version` VALUES ('c8450cdfd896');
+INSERT INTO `alembic_version` VALUES ('4a2dabf89daa');
 
 -- ----------------------------
 -- Table structure for alumnos
@@ -54,5 +54,25 @@ INSERT INTO `alumnos` VALUES (11, 'naomi', 'naomi@gmail.com', '2026-02-24 20:19:
 INSERT INTO `alumnos` VALUES (12, 'Victor', 'victor@gmail.com', '2026-02-24 20:20:03', 'Cuellar', '47712345657');
 INSERT INTO `alumnos` VALUES (13, 'Mayra', 'mayra@gmail.com', '2026-02-24 20:51:23', 'hernandez', '4771234567');
 INSERT INTO `alumnos` VALUES (14, 'hola', 'saul@gmail.com', '2026-02-24 20:51:41', 'Villeda', '4771234567');
+
+-- ----------------------------
+-- Table structure for maestros
+-- ----------------------------
+DROP TABLE IF EXISTS `maestros`;
+CREATE TABLE `maestros`  (
+  `matricula` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `apellidos` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `especialidad` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `email` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`matricula`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of maestros
+-- ----------------------------
+INSERT INTO `maestros` VALUES (2, 'Rosa Fernanda', 'Martinez Orozco', 'Español', 'rosa@gmail.com');
+INSERT INTO `maestros` VALUES (5, 'Marcos', 'Baños', 'Matemáticas', 'marcos@gmail.com');
+INSERT INTO `maestros` VALUES (16, 'Martha', 'Baños', 'Marketing', 'martha@gmail.com');
 
 SET FOREIGN_KEY_CHECKS = 1;
